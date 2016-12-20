88e88c5a313cdb4019122d3cb1298e463f86f0e6
Marin Piskač <marin.piskac@i-ways.hr>
2016-12-20 22:58:46 +0100

    jos logina

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,20 @@
 @section('content')
 <div class="login-page">
     <div class="form">
-        <form class="login-form">
-            <input type="email" placeholder="e-mail" id="email"/>
-            <input type="password" placeholder="password" id="password"/>
-            <h6 class="h6" id="message"></h6>
+        <form class="login-form" action="/login" method="post">
+            {{csrf_field()}}
+            <input type="email" placeholder="e-mail" name="email" id="email" value="{{ old('email') }}"/>
+            <input type="password" placeholder="password" name="password" id="password"/>
+            <h6 class="h6" id="message">
+                @foreach ($errors->all() as $error)
+                {{$error}}
+                @endforeach
+            </h6>
             <div id="login_button">
                 <button type="submit">login</button>
             </div>
         </form>
     </div>
 </div>
+
 @endsection
